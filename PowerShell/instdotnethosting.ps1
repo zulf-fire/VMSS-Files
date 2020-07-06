773b054e390dd2b623cc24274c517806c59664c4
zulf-fire <62550216+zulf-fire@users.noreply.github.com>
2020-07-06 17:43:28 +0530

    Add files via upload

---
+++ PowerShell/instdotnethosting.ps1
@@ -0,0 +1,7 @@
+﻿Invoke-WebRequest -Uri 'https://download.visualstudio.microsoft.com/download/pr/ff658e5a-c017-4a63-9ffe-e53865963848/15875eef1f0b8e25974846e4a4518135/dotnet-hosting-3.1.3-win.exe' -OutFile 'c:\temp\dotnet-hosting-3.1.3-win.exe'
+
+Start-Sleep -Seconds 10
+
+Unblock-File -Path C:\temp\dotnet-hosting-3.1.3-win.exe
+
+Invoke-Expression -Command 'C:\temp\dotnet-hosting-3.1.3-win.exe /install /quiet /norestart'
